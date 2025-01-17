fae3ea88c8d76e22cd9912972a4989af7ea1b350
Felipe Toffetti <139416594+toffettl@users.noreply.github.com>
2025-01-17 10:35:21 -0300

    Exercício finalizado

---
+++ Ex.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+sobrenome = "Toffetti"
+idade = 16
+ano_nascimento = 2008
+maior_de_idade = idade <= 18
+altura_metros = 1.78
+
+print('Nome:', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade:', idade)
+print("Ano de nascimento :", ano_nascimento)
+print('É maior de idade?: ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
